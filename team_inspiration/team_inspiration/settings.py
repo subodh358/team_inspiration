e587b0d6313aa2386091e189aa3f496eb07f1e72
subodh <subodhpopalwar.345@gmail.com>
2020-10-11 23:34:48 +0530

    working

--- team_inspiration/team_inspiration/settings.py
+++ team_inspiration/team_inspiration/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'core',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -97,9 +100,7 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
